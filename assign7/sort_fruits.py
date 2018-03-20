0d7a2af64e6ad652a3bc8346173a9e0d354ae104
thaissa.falbo <thaissa.falbo@gmail.com>
2018-03-20 19:20:10 -0300

    Finish Python script

--- assign7/sort_fruits.py
+++ assign7/sort_fruits.py
@@ -1,14 +1,29 @@
 ## Sorting Fruits
 
+#Open files for input and output
 infile = open("unsorted_fruits.txt", "r")
-# outfile=open("sorted_fruits.txt","w")
+outfile=open("sorted_fruits.txt","w")
 
+# Creates empty list for receiving the fruit words from input file
+list_of_fruits = []
+
+# Read every line in file
 for line in infile:
-    print(line)
+    # Read every word in line (ignoring white spaces)
+    for word in line.split():
+        # Append word to list
+        list_of_fruits.append(word)
 
 
-# fruit=infile.read(50)
-# outfile.write(fruit)
-# print (fruit)
-# infile.close()
-# outfile.close()
+# Create new list with sorted words
+sorted_list_of_fruits = sorted(list_of_fruits)
+
+# Write sorted words in output file
+for fruit in sorted_list_of_fruits:
+    outfile.write(fruit+"\n")
+
+# Close files
+infile.close()
+outfile.close()
+
+
